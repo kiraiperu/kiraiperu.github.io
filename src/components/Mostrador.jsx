9e90b96384c7a602dfabb0ce2f41965834d77839
kiraiperu <kirai.peru@gmail.com>
2022-10-10 07:49:05 -0500

    jajadiff

--- src/components/Mostrador.jsx
+++ src/components/Mostrador.jsx
@@ -8,7 +8,6 @@ import {useState} from "react"
 const Mostrador = (props) => {
 
     const [index, setIndex] = useState(0)
-    const [showimg, setShowimg] = useState(props.designs[index].img);
 
     const handleLeft = () => {
         if(index-1 < 0) {
@@ -17,7 +16,6 @@ const Mostrador = (props) => {
         else {
             setIndex((prev) => prev-1)
         }
-        setShowimg(props.designs[index].img)
     }
 
     const handleRight = () => {
@@ -27,14 +25,13 @@ const Mostrador = (props) => {
         else {
             setIndex((prev) => prev+1)
         }
-        setShowimg(props.designs[index].img)
     }
 
     return(
         <div className="">
             <div className="mostradorcont">
                 <img className="leftarr" src={leftimg} onClick={handleLeft}></img>
-                <img className="mostimg" src={showimg}></img>
+                <img className="mostimg" src={props.designs[index].img}></img>
                 <img className="rightarr" src={rightimg} onClick={handleRight}></img>
             </div>      
             <nav className="linea">NUESTROS DISEÑOS</nav>
