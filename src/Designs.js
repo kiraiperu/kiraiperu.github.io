1e973f3b94ccddcaa728727849ed18e6b0b81e65
kiraiperu <kirai.peru@gmail.com>
2022-10-11 04:59:06 -0500

    kiraiperu

--- src/Designs.js
+++ src/Designs.js
@@ -3,9 +3,9 @@ import yasuo from "./assets/yasuo.png"
 import freedom from "./assets/freedom.png"
 
 const designs = [
-    {id: 1, img: yasuo, oldprice: 40, price: 35, text:"Polo Kimetsu"},
-    {id: 2, img: freedom,  oldprice: 40, price: 35, text:"Polo Eren"},
-    {id: 3, img: eva, oldprice: 40, price: 35, text:"Polo EVA01"}
+    {id: 1, img: yasuo, oldprice: 40, price: 35, text:"T-SHIRT KIMETSU"},
+    {id: 2, img: freedom,  oldprice: 40, price: 35, text:"T-SHIRT EREN"},
+    {id: 3, img: eva, oldprice: 40, price: 35, text:"T-SHIRT EVA01"}
 ]
 
 export default designs
